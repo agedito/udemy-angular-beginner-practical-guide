7cdce2f889111f3f82ba7f1a9cc2c190e7d1839c
agedito <agedito@gmail.com>
2022-12-20 17:28:52 +0100

    empty homes and movies pages

--- movies-app/src/app/app.module.ts
+++ movies-app/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shader/header/header.component';
 import { FooterComponent } from './shader/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
