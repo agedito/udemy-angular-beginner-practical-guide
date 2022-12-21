921e93a749e0511f9fed5996d51f3ece43fb84f5
agedito <agedito@gmail.com>
2022-12-21 03:45:55 +0100

    slider data mock

--- movies-app/src/app/components/slider/slider.component.ts
+++ movies-app/src/app/components/slider/slider.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-slider',
-  templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+	selector: "slider",
+	templateUrl: "./slider.component.html",
+	styleUrls: ["./slider.component.scss"]
 })
-export class SliderComponent {
-
-}
+export class SliderComponent {}
