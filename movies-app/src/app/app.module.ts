19647090663712502649f0712271940ec4ebe34a
agedito <agedito@gmail.com>
2022-12-23 23:43:31 +0100

    renaming

--- movies-app/src/app/app.module.ts
+++ movies-app/src/app/app.module.ts
@@ -10,8 +10,8 @@ import { MoviesComponent } from "./pages/movies/movies.component";
 import { HttpClientModule } from "@angular/common/http";
 import { SliderComponent } from "./components/slider/slider.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { ItemsBannerComponent } from './components/items-banner/items-banner.component';
-import { MovieItemComponent } from './components/movie-item/movie-item.component';
+import { ItemsBannerComponent } from "./components/items-banner/items-banner.component";
+import { MovieItemComponent } from "./components/item/item.component";
 
 @NgModule({
 	declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, MoviesComponent, SliderComponent, ItemsBannerComponent, MovieItemComponent],
